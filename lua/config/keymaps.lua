0e7f7a64a6f30e601d798d6bd0d8e327ce7d348d
Theo <quangthang99@gmail.com>
2025-08-19 14:55:33 +0700

    keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -15,3 +15,5 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+vim.keymap.set('t', '<esc><esc>', '<c-\\><c-n>')
