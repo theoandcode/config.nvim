a2b1afb6e05e1cde8303ca00af6f2afcf6bbc770
Theo <quangthang99@gmail.com>
2025-09-12 14:58:45 +0700

    remove redundant config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -90,15 +90,6 @@ vim.diagnostic.config {
   virtual_text = {
     source = 'if_many',
     spacing = 2,
-    format = function(diagnostic)
-      local diagnostic_message = {
-        [vim.diagnostic.severity.ERROR] = diagnostic.message,
-        [vim.diagnostic.severity.WARN] = diagnostic.message,
-        [vim.diagnostic.severity.INFO] = diagnostic.message,
-        [vim.diagnostic.severity.HINT] = diagnostic.message,
-      }
-      return diagnostic_message[diagnostic.severity]
-    end,
   },
   virtual_lines = {
     current_line = true,
