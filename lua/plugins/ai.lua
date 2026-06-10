d0f4dc014c4a062b7b11cc346cd54e5c0e47ac3f
Theo <quangthang99@gmail.com>
2026-06-10 13:31:09 +0700

    remove some copilot plugins

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -136,36 +136,4 @@ return {
       -- 'nvim_mini/mini.nvim',
     },
   },
-  {
-    enabled = false,
-    'github/copilot.vim',
-    event = 'VeryLazy',
-    -- Lazy loading copilot only when <leader>ct is pressed
-    keys = {
-      {
-        '<leader>ct',
-        function()
-          vim.g.copilot_enabled = not vim.g.copilot_enabled
-          vim.cmd 'Copilot'
-        end,
-        desc = '[T]oggle Copilot',
-      },
-    },
-    init = function()
-      vim.g.copilot_enabled = false
-    end,
-  },
-  {
-    {
-      'CopilotC-Nvim/CopilotChat.nvim',
-      dependencies = {
-        { 'nvim-lua/plenary.nvim', branch = 'master' },
-      },
-      build = 'make tiktoken',
-      keys = {
-        { '<leader>cc', ':CopilotChat<cr>', desc = 'Open [C]opilot [C]chat', silent = true, mode = { 'n', 'v' } },
-      },
-      opts = { auto_insert_mode = true },
-    },
-  },
 }
